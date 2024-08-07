12291159726ebc4b885d7f28824298ebb3bf98f5
ehtisham mohd <125719040+Ehtisham9198@users.noreply.github.com>
2024-08-07 23:06:10 +0530

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
     return (
         <div className='footer' style={{position: 'relative', bottom:0, width: '100%'}}>
-            <h4>All Right Reserved &copy; My App </h4>
+            <h4>All Right Reserved &copy; TourMitr </h4>
              <p>
                 <Link to='/about' className='ml-2 mr-2'>About us</Link>
                 |
